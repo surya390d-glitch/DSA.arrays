59404b8bf3b5f446ad63ed3c44617390b8717550
Surya Devi <surya390d@gmail.com>
2026-02-09 07:22:36 +0530

    Correct typos and enhance array syntax examples
    Fixed typos and added examples for array syntax.

---
+++ ArraySyntax.java
@@ -0,0 +1,13 @@
+class Syntax{
+   public static void main(String args[]){
+     // datatype [] variable_name = new datatype [size];
+                        //or
+     // datatype [] variable_name = {store int or "string};
+     //for example(int) 
+     int [] num = new int[5];
+     int [] num2 = {1,2,3,4,5,6,7};
+     //example for string
+     String [] names = {"trump","elon","epstein","hawking"};
+     Sysyem.out.println("names: " + names);
+   }
+}
